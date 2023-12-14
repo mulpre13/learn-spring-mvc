a40a03291f939091dc360896a7ccfc74a5fc108e
woosung-21 <woosung.21@navercorp.com>
2023-12-14 11:13:34 +0900

    init project

---
+++ src/test/kotlin/woosung/learn/springmvc/SpringMvcApplicationTests.kt
@@ -0,0 +1,13 @@
+package woosung.learn.springmvc
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringMvcApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
